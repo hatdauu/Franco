be362039589c5df24b32b8e714c015be87d5ec62
Nguyen Son <hatdau2k2@gmail.com>
2023-02-28 16:20:42 +0700

    hihi

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.blog__container-inner-list').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+});
+      
